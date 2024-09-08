cbc46ad46e333b87876b1fe53544e82608eb6254
NastyaParnet <nastya.parnet.01@gmail.com>
2024-09-08 10:40:55 +0300

    add productModel

---
+++ models/productModel.js
@@ -0,0 +1,32 @@
+const { Sequelize } = require("sequelize");
+const db = require("../config/db");
+ 
+const { DataTypes } = Sequelize;
+ 
+const Product = db.define('product',{
+  id:{
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title:{
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price:{
+    type: DataTypes.FLOAT(10, 2),
+  },
+  description:{
+    type: DataTypes.STRING
+  },
+  createdAt:{
+    type: DataTypes.DATE
+  },
+  updatedAt:{
+    type: DataTypes.DATE
+  }
+},{
+  freezeTableName: true
+});
+ 
+module.exports = Product;
